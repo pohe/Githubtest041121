4af6066c93594e05b1b6e8a180ac8fe9460725e6
Poul Henriksen <pohe@easj.dk>
2021-11-04 09:56:57 +0100

    Add project files.

---
+++ GithubTestProject/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GithubTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
