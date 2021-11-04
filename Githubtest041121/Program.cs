70c810adc23a5510eaecd2997b9906be29569405
Poul Henriksen <pohe@easj.dk>
2021-11-04 10:11:37 +0100

    Tilføjet lokal kommentar

--- Githubtest041121/Program.cs
+++ Githubtest041121/Program.cs
@@ -7,6 +7,7 @@ namespace Githubtest041121
         static void Main(string[] args)
         {
             // Dette er en kommentar der er lavet på Github serveren
+            // Dette er en kommentar lavet på clienten/lokalt
             Console.WriteLine("Hello World! Welcome to Github");
 
         }
